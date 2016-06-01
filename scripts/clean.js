9fb41153679af274066dc5eebe5497c8f0f35584
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2016-06-01 16:21:42 -0400

    Clean task should not error out when deployed directories do not exist

--- scripts/clean.js
+++ scripts/clean.js
@@ -2,18 +2,34 @@ var fs = require('fs');
 var path = require('path');
 var rimraf = require('rimraf');
 
-var fsItems = fs.readdirSync(path.join(__dirname, './../src'));
-fsItems = fsItems.filter(isDirectory());
+var appRootDirectory = path.join(__dirname, '..');
+var rootSrcDirectory = path.join(appRootDirectory, 'src');
+var fsItems = fs.readdirSync(rootSrcDirectory);
+fsItems = fsItems
+    .filter(isDirectory(rootSrcDirectory))
+    .map(toDeployedDirectory(appRootDirectory))
+    .filter(hasMatchingDeployedDirectory())
+    .forEach(deleteDeployedDirectory(appRootDirectory));
 
-fsItems.forEach(deleteDist(path.join(__dirname, './../')));
-
-function isDirectory() {
+function isDirectory(rootDirectory) {
     return function(itemRef) {
-        return fs.statSync(itemRef).isDirectory();
+        return fs.statSync(path.join(rootDirectory, itemRef)).isDirectory();
     }
 }
 
-function deleteDist(rootDirectory) {
+function toDeployedDirectory(deployRootDirectory) {
+    return function(directoryRef) {
+        return path.join(deployRootDirectory, directoryRef);
+    };
+}
+
+function hasMatchingDeployedDirectory() {
+    return function(directoryRef) {
+        return fs.existsSync(directoryRef);
+    };
+}
+
+function deleteDeployedDirectory(rootDirectory) {
     return function(directoryRef) {
         rimraf(path.join(rootDirectory, directoryRef), logDone);
     }

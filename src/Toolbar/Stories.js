9d4099e242d8addf33bc7d961af6fd0950e6a39c
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2016-06-01 16:42:42 -0400

    Include more stories for Toolbar

--- src/Toolbar/Stories.js
+++ src/Toolbar/Stories.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import {storiesOf, action} from '@kadira/storybook';
-
+import {storiesOf} from '@kadira/storybook';
 import Toolbar from './Toolbar';
+import ToolbarGroup from './ToolbarGroup';
+import ToolbarGroupTitle from './ToolbarGroupTitle';
 
 storiesOf('Toolbar', module)
     .add('with title', () => (
-        <Toolbar text="My Title" />
+        <Toolbar title="App Toolbar" />
     ))
-    .add('with no text', () => (
-        <Toolbar text="My Title" />
+    .add('with no title', () => (
+        <Toolbar />
+    ))
+    .add('kitchen sink', () => (
+        <Toolbar title="App Toolbar">
+            <ToolbarGroup>
+                <button />
+            </ToolbarGroup>
+        </Toolbar>
+    ));
+
+storiesOf('ToolbarGroup', module)
+    .add('with title', () => (
+        <ToolbarGroup>
+            <ToolbarGroupTitle text="Group title" />
+        </ToolbarGroup>
+    ));
+
+storiesOf('ToolbarGroupTitle', module)
+    .add('with text', () => (
+        <ToolbarGroupTitle text="Group title" />
     ));

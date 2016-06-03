9db901e7ca4e8c3774c68d0b155f453e2a5895d0
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2016-06-03 09:57:30 -0400

    Minor clean-up in spec verbiage

--- src/Toolbar/Toolbar.spec.js
+++ src/Toolbar/Toolbar.spec.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import Toolbar from './Toolbar';
+import ToolbarGroup from './ToolbarGroup';
 
 describe('<Toolbar />', function() {
-    describe('when rendering a Toolbar with no title', () => {
+    describe('when rendering a toolbar with no title', () => {
         beforeEach(() => {
             this.wrapper = shallow(<Toolbar />);
         });
@@ -12,7 +13,7 @@ describe('<Toolbar />', function() {
             this.wrapper.find('h1').length.should.equal(0);
         });
     });
-    describe('when rendering a Toolbar with a title', () => {
+    describe('when rendering a toolbar with a title', () => {
         beforeEach(() => {
             this.wrapper = shallow(<Toolbar title="Toolbar Title" />);
         });
@@ -23,11 +24,11 @@ describe('<Toolbar />', function() {
         });
     });
 
-    describe('when rendering a Toolbar with a title and a custom title style', () => {
+    describe('when rendering a toolbar with a title and a custom title style', () => {
         beforeEach(() => {
             this.wrapper = shallow(<Toolbar title="Toolbar Title" titleStyle={{display: 'none'}} />);
         });
-        it('it should render the Toolbar with the titleStyle prop applied to the title', () => {
+        it('it should render the toolbar with the titleStyle prop applied to the title', () => {
             expect(this.wrapper.find('h1')).to.have.style('display', 'none');
         });
     });
@@ -36,7 +37,7 @@ describe('<Toolbar />', function() {
         beforeEach(() => {
             this.wrapper = shallow(<Toolbar className="my-class-name" />);
         });
-        it('it should render the Toolbar with specified className on the root element', () => {
+        it('it should render the toolbar with specified className on the root element', () => {
             this.wrapper.find('.toolbar').hasClass('my-class-name');
         });
     });
@@ -45,24 +46,24 @@ describe('<Toolbar />', function() {
         beforeEach(() => {
             this.wrapper = shallow(<Toolbar style={{display: 'inline'}} titleStyle={{display: 'none'}} />);
         });
-        it('it should render the Toolbar with the style prop applied to the root element', () => {
+        it('it should render the toolbar with the style prop applied to the root element', () => {
             expect(this.wrapper).to.have.style('display', 'inline');
         });
     });
 
-    describe('when rendering a Toolbar with children and a title', () => {
+    describe('when rendering a toolbar with children and a title', () => {
         beforeEach(() => {
             this.wrapper = shallow(
                 <Toolbar title="Toolbar Title">
-                    <div className="children1"></div>
-                    <div className="children2"></div>
+                    <ToolbarGroup />
+                    <ToolbarGroup />
                 </Toolbar>
             );
         });
         it('it should render the children in a grouping div', () => {
             expect(this.wrapper.childAt(1).children().length).to.equal(2);
-            expect(this.wrapper.childAt(1)).to.have.descendants('.children1');
-            expect(this.wrapper.childAt(1)).to.have.descendants('.children2');
+            expect(this.wrapper.childAt(1)).to.have.descendants(ToolbarGroup);
+            expect(this.wrapper.childAt(1)).to.have.descendants(ToolbarGroup);
         });
         it('it should render grouping of children after title', () => {
             expect(this.wrapper.childAt(0).type()).to.equal('h1');

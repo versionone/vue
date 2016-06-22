de0ab1a2d90e18a5fe04f6834d5b3547fec4c34e
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2016-06-22 08:59:31 -0400

    #3 Update specs for theming mechanism on toolbar

--- src/Toolbar/Toolbar.spec.js
+++ src/Toolbar/Toolbar.spec.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {mount} from 'enzyme';
 import Toolbar from './Toolbar';
+import ThemeProvider from './../styles/ThemeProvider';
 
 describe('<Toolbar />', function() {
     describe('when rendering a toolbar without specifying the height style', () => {
         beforeEach(() => {
-            this.wrapper = shallow(<Toolbar />);
+            this.wrapper = mount(<ThemeProvider><Toolbar /></ThemeProvider>);
         });
         it('it should use the default height of 56px', () => {
             this.wrapper.should.have.style('height', '56px');
@@ -14,25 +15,16 @@ describe('<Toolbar />', function() {
 
     describe('when rendering a toolbar with a specified height', () => {
         beforeEach(() => {
-            this.wrapper = shallow(<Toolbar style={{height: '100px'}} />);
+            this.wrapper = mount(<ThemeProvider><Toolbar style={{height: '100px'}} /></ThemeProvider>);
         });
         it('it should use the style height for the Toolbar\'s height', () => {
             this.wrapper.should.have.style('height', '100px');
         });
     });
 
-    describe('when rendering a toolbar', () => {
-        beforeEach(() => {
-            this.wrapper = shallow(<Toolbar><span></span></Toolbar>);
-        });
-        it('it should render it\'s children with the Toolbar\'s height', () => {
-            this.wrapper.find('.toolbar').children().forEach((child) => child.should.have.style('height', '56px'));
-        });
-    });
-
     describe('when rendering a toolbar with a className', () => {
         beforeEach(() => {
-            this.wrapper = shallow(<Toolbar className="my-class-name" />);
+            this.wrapper = mount(<ThemeProvider><Toolbar className="my-class-name" /></ThemeProvider>);
         });
         it('it should render the toolbar with specified className on the root element', () => {
             this.wrapper.find('.toolbar').should.have.className('my-class-name');
@@ -41,7 +33,7 @@ describe('<Toolbar />', function() {
 
     describe('when rendering a toolbar with a custom style', () => {
         beforeEach(() => {
-            this.wrapper = shallow(<Toolbar style={{display: 'inline'}} />);
+            this.wrapper = mount(<ThemeProvider><Toolbar style={{display: 'inline'}} /></ThemeProvider>);
         });
         it('it should render the toolbar with the style prop applied to the root element', () => {
             this.wrapper.should.have.style('display', 'inline');
@@ -50,19 +42,21 @@ describe('<Toolbar />', function() {
 
     describe('when rendering a toolbar with children', () => {
         beforeEach(() => {
-            this.wrapper = shallow(
-                <Toolbar title="Toolbar Title" style={{height: '56px'}}>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </Toolbar>
+            this.wrapper = mount(
+                <ThemeProvider>
+                    <Toolbar title="Toolbar Title" style={{height: '56px'}}>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </Toolbar>
+                </ThemeProvider>
             );
         });
         it('it should render the children', () => {
             this.wrapper.children().length.should.equal(3);
         });
         it('it should render the children with the toolbar\'s height', () => {
-            this.wrapper.children().forEach((child)=> child.should.have.style('height', '56px'));
+            this.wrapper.find('.toolbar').children().forEach((child)=> child.should.have.style('height', '56px'));
         });
     });
 });

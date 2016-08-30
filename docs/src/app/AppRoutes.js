a2893762d0416566c82b5243beee3f01a6eff87c
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2016-08-30 16:01:42 -0400

    Removal of routes that have been deleted

--- docs/src/app/AppRoutes.js
+++ docs/src/app/AppRoutes.js
@@ -9,12 +9,6 @@ import {
 import Master from './components/Master';
 import Home from './components/pages/Home';
 
-import Prerequisites from './components/pages/get-started/Prerequisites';
-import Installation from './components/pages/get-started/Installation';
-import Usage from './components/pages/get-started/Usage';
-import Examples from './components/pages/get-started/Examples';
-import ServerRendering from './components/pages/get-started/ServerRendering';
-
 import Colors from './components/pages/Patterns/Colors';
 import Themes from './components/pages/Patterns/Themes';
 import Styles from './components/pages/Patterns/Styles';
@@ -34,13 +28,6 @@ const AppRoutes = (
     <IndexRoute component={Home} />
     <Route path="home" component={Home} />
     <Redirect from="get-started" to="/get-started/prerequisites" />
-    <Route path="get-started">
-      <Route path="prerequisites" component={Prerequisites} />
-      <Route path="installation" component={Installation} />
-      <Route path="usage" component={Usage} />
-      <Route path="examples" component={Examples} />
-      <Route path="server-rendering" component={ServerRendering} />
-    </Route>
     <Redirect from="patterns" to="/patterns/themes" />
     <Route path="Patterns">
       <Route ptah="colors" component={Colors} />

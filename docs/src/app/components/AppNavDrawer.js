1c37da148a6ffce0d230bf813c03ed1a3ddbd651
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2016-08-31 15:20:40 -0400

    Reformatting

--- docs/src/app/components/AppNavDrawer.js
+++ docs/src/app/components/AppNavDrawer.js
@@ -12,195 +12,200 @@ import SearchField from './SearchField';
 const SelectableList = MakeSelectable(List);
 
 const styles = {
-  logo: {
-    cursor: 'pointer',
-    fontSize: 24,
-    color: typography.textFullWhite,
-    lineHeight: `${spacing.desktopKeylineIncrement}px`,
-    fontWeight: typography.fontWeightLight,
-    backgroundColor: gunSmoke,
-    paddingLeft: spacing.desktopGutter,
-    marginBottom: 8,
-  },
-  version: {
-    paddingLeft: spacing.desktopGutterLess,
-    fontSize: 16,
-  },
+    logo: {
+        cursor: 'pointer',
+        fontSize: 24,
+        color: typography.textFullWhite,
+        lineHeight: `${spacing.desktopKeylineIncrement}px`,
+        fontWeight: typography.fontWeightLight,
+        backgroundColor: gunSmoke,
+        paddingLeft: spacing.desktopGutter,
+        marginBottom: 8,
+    },
+    version: {
+        paddingLeft: spacing.desktopGutterLess,
+        fontSize: 16,
+    },
+    search: {
+        paddingLeft: spacing.desktopGutterLess,
+        paddingRight: spacing.desktopGutterLess,
+        fontSize: 16,
+    },
 };
 
 class AppNavDrawer extends Component {
-  static propTypes = {
-    docked: PropTypes.bool.isRequired,
-    location: PropTypes.object.isRequired,
-    onChangeList: PropTypes.func.isRequired,
-    onRequestChangeNavDrawer: PropTypes.func.isRequired,
-    open: PropTypes.bool.isRequired,
-    style: PropTypes.object,
-  };
-
-  static contextTypes = {
-    muiTheme: PropTypes.object.isRequired,
-    router: PropTypes.object.isRequired,
-  };
-
-  state = {
-    muiVersions: [],
-  };
-
-  componentDidMount() {
-    const self = this;
-    const url = '/versions.json';
-    const request = new XMLHttpRequest();
-
-    request.onreadystatechange = function() {
-      if (request.readyState === 4 && request.status === 200) {
-        self.setState({
-          muiVersions: JSON.parse(request.responseText),
-          version: JSON.parse(request.responseText)[0],
-        });
-      }
+    static propTypes = {
+        docked: PropTypes.bool.isRequired,
+        location: PropTypes.object.isRequired,
+        onChangeList: PropTypes.func.isRequired,
+        onRequestChangeNavDrawer: PropTypes.func.isRequired,
+        open: PropTypes.bool.isRequired,
+        style: PropTypes.object,
     };
 
-    request.open('GET', url, true);
-    request.send();
-  }
+    static contextTypes = {
+        muiTheme: PropTypes.object.isRequired,
+        router: PropTypes.object.isRequired,
+    };
 
-  firstNonPreReleaseVersion() {
-    let version;
-    for (let i = 0; i < this.state.muiVersions.length; i++) {
-      version = this.state.muiVersions[i];
-      // If the version doesn't contain '-' and isn't 'HEAD'
-      if (!/-/.test(version) && version !== 'HEAD') {
-        break;
-      }
+    state = {
+        muiVersions: [],
+    };
+
+    componentDidMount() {
+        const self = this;
+        const url = '/versions.json';
+        const request = new XMLHttpRequest();
+
+        request.onreadystatechange = function() {
+            if (request.readyState === 4 && request.status === 200) {
+                self.setState({
+                    muiVersions: JSON.parse(request.responseText),
+                    version: JSON.parse(request.responseText)[0],
+                });
+            }
+        };
+
+        request.open('GET', url, true);
+        request.send();
     }
-    return version;
-  }
 
-  handleVersionChange = (event, index, value) => {
-    if (value === this.firstNonPreReleaseVersion()) {
-      window.location = 'http://www.material-ui.com/';
-    } else {
-      window.location = `http://www.material-ui.com/${value}`;
+    firstNonPreReleaseVersion() {
+        let version;
+        for (let i = 0; i < this.state.muiVersions.length; i++) {
+            version = this.state.muiVersions[i];
+            // If the version doesn't contain '-' and isn't 'HEAD'
+            if (!/-/.test(version) && version !== 'HEAD') {
+                break;
+            }
+        }
+        return version;
     }
-  };
 
-  currentVersion() {
-    if (window.location.hostname === 'localhost') return this.state.muiVersions[0];
-    if (window.location.pathname === '/') {
-      return this.firstNonPreReleaseVersion();
-    } else {
-      return window.location.pathname.replace(/\//g, '');
+    handleVersionChange = (event, index, value) => {
+        if (value === this.firstNonPreReleaseVersion()) {
+            window.location = 'http://www.material-ui.com/';
+        } else {
+            window.location = `http://www.material-ui.com/${value}`;
+        }
+    };
+
+    currentVersion() {
+        if (window.location.hostname === 'localhost') return this.state.muiVersions[0];
+        if (window.location.pathname === '/') {
+            return this.firstNonPreReleaseVersion();
+        } else {
+            return window.location.pathname.replace(/\//g, '');
+        }
     }
-  }
 
-  handleRequestChangeLink = (event, value) => {
-    window.location = value;
-  };
+    handleRequestChangeLink = (event, value) => {
+        window.location = value;
+    };
 
-  handleTouchTapHeader = () => {
-    this.context.router.push('/');
-    this.props.onRequestChangeNavDrawer(false);
-  };
+    handleTouchTapHeader = () => {
+        this.context.router.push('/');
+        this.props.onRequestChangeNavDrawer(false);
+    };
 
-  render() {
-    const {
-      location,
-      docked,
-      onRequestChangeNavDrawer,
-      onChangeList,
-      open,
-      style,
-    } = this.props;
+    render() {
+        const {
+            location,
+            docked,
+            onRequestChangeNavDrawer,
+            onChangeList,
+            open,
+            style,
+        } = this.props;
 
-    return (
-      <Drawer
-        style={style}
-        docked={docked}
-        open={open}
-        onRequestChange={onRequestChangeNavDrawer}
-        containerStyle={{zIndex: zIndex.drawer - 100}}
-      >
-        <div style={styles.logo} onTouchTap={this.handleTouchTapHeader}>
-          VersionOne-UI
-        </div>
-        <span style={styles.version}>Version:</span>
-        <DropDownMenu
-          value={this.currentVersion()}
-          onChange={this.handleVersionChange}
-          maxHeight={300}
-          style={{width: 181}}
-        >
-          {this.state.muiVersions.map((version) => (
-            <MenuItem
-              key={version}
-              value={version}
-              primaryText={version}
-            />
-          ))}
-        </DropDownMenu>
-          <div style={styles.version}>
-            <SearchField />
-          </div>
-        <SelectableList
-          value={location.pathname}
-          onChange={onChangeList}
-        >
-          <ListItem
-            primaryText="Get Started"
-            primaryTogglesNestedList={true}
-            nestedItems={[
-              <ListItem primaryText="Prerequisites" value="/get-started/prerequisites" />,
-              <ListItem primaryText="Installation" value="/get-started/installation" />,
-              <ListItem primaryText="Usage" value="/get-started/usage" />,
-              <ListItem primaryText="Server Rendering" value="/get-started/server-rendering" />,
-              <ListItem primaryText="Examples" value="/get-started/examples" />,
-            ]}
-          />
-          <ListItem
-            primaryText="Customization"
-            primaryTogglesNestedList={true}
-            nestedItems={[
-              <ListItem primaryText="Themes" value="/customization/themes" />,
-              <ListItem primaryText="Styles" value="/customization/styles" />,
-              <ListItem primaryText="Colors" value="/customization/colors" />,
-            ]}
-          />
-          <ListItem
-            primaryText="Components"
-            primaryTogglesNestedList={true}
-            nestedItems={[
-              <ListItem
-                primaryText="Popover"
-                value="/components/popover"
-                href="#/components/popover"
-              />
-            ]}
-          />
-          <ListItem
-            primaryText="Discover More"
-            primaryTogglesNestedList={true}
-            nestedItems={[
-              <ListItem primaryText="Community" value="/discover-more/community" />,
-              <ListItem primaryText="Contributing" value="/discover-more/contributing" />,
-              <ListItem primaryText="Showcase" value="/discover-more/showcase" />,
-              <ListItem primaryText="Related projects" value="/discover-more/related-projects" />,
-            ]}
-          />
-        </SelectableList>
-        <Divider />
-        <SelectableList
-          value=""
-          onChange={this.handleRequestChangeLink}
-        >
-          <Subheader>Resources</Subheader>
-          <ListItem primaryText="GitHub" value="https://github.com/versionone/versionone-ui" />
-          <ListItem primaryText="React" value="http://facebook.github.io/react" />
-          <ListItem primaryText="VersionOne" value="http://VersionOne.com" />
-        </SelectableList>
-      </Drawer>
-    );
-  }
+        return (
+            <Drawer
+                style={style}
+                docked={docked}
+                open={open}
+                onRequestChange={onRequestChangeNavDrawer}
+                containerStyle={{zIndex: zIndex.drawer - 100}}
+            >
+                <div style={styles.logo} onTouchTap={this.handleTouchTapHeader}>
+                    VersionOne-UI
+                </div>
+                <span style={styles.version}>Version:</span>
+                <DropDownMenu
+                    value={this.currentVersion()}
+                    onChange={this.handleVersionChange}
+                    maxHeight={300}
+                    style={{width: 181}}
+                >
+                    {this.state.muiVersions.map((version) => (
+                        <MenuItem
+                            key={version}
+                            value={version}
+                            primaryText={version}
+                        />
+                    ))}
+                </DropDownMenu>
+                <div style={styles.search}>
+                    <SearchField />
+                </div>
+                <SelectableList
+                    value={location.pathname}
+                    onChange={onChangeList}
+                >
+                    <ListItem
+                        primaryText="Get Started"
+                        primaryTogglesNestedList={true}
+                        nestedItems={[
+                            <ListItem primaryText="Prerequisites" value="/get-started/prerequisites" />,
+                            <ListItem primaryText="Installation" value="/get-started/installation" />,
+                            <ListItem primaryText="Usage" value="/get-started/usage" />,
+                            <ListItem primaryText="Server Rendering" value="/get-started/server-rendering" />,
+                            <ListItem primaryText="Examples" value="/get-started/examples" />,
+                        ]}
+                    />
+                    <ListItem
+                        primaryText="Customization"
+                        primaryTogglesNestedList={true}
+                        nestedItems={[
+                            <ListItem primaryText="Themes" value="/customization/themes" />,
+                            <ListItem primaryText="Styles" value="/customization/styles" />,
+                            <ListItem primaryText="Colors" value="/customization/colors" />,
+                        ]}
+                    />
+                    <ListItem
+                        primaryText="Components"
+                        primaryTogglesNestedList={true}
+                        nestedItems={[
+                            <ListItem
+                                primaryText="Popover"
+                                value="/components/popover"
+                                href="#/components/popover"
+                            />
+                        ]}
+                    />
+                    <ListItem
+                        primaryText="Discover More"
+                        primaryTogglesNestedList={true}
+                        nestedItems={[
+                            <ListItem primaryText="Community" value="/discover-more/community" />,
+                            <ListItem primaryText="Contributing" value="/discover-more/contributing" />,
+                            <ListItem primaryText="Showcase" value="/discover-more/showcase" />,
+                            <ListItem primaryText="Related projects" value="/discover-more/related-projects" />,
+                        ]}
+                    />
+                </SelectableList>
+                <Divider />
+                <SelectableList
+                    value=""
+                    onChange={this.handleRequestChangeLink}
+                >
+                    <Subheader>Resources</Subheader>
+                    <ListItem primaryText="GitHub" value="https://github.com/versionone/versionone-ui" />
+                    <ListItem primaryText="React" value="http://facebook.github.io/react" />
+                    <ListItem primaryText="VersionOne" value="http://VersionOne.com" />
+                </SelectableList>
+            </Drawer>
+        );
+    }
 }
 
 export default AppNavDrawer;

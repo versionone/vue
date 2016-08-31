f4e43f67f4c816ecf99b3ab02421d6733789c606
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2016-08-31 12:50:01 -0400

    Add example and proptypes descriptions

--- docs/src/app/components/pages/components/Popover/ExampleSimple.js
+++ docs/src/app/components/pages/components/Popover/ExampleSimple.js
@@ -1,57 +1,49 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
-import Popover from 'material-ui/Popover';
-import Menu from 'material-ui/Menu';
-import MenuItem from 'material-ui/MenuItem';
+import Popover from 'versionone-ui/Popover';
 
 export default class PopoverExampleSimple extends React.Component {
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      open: false,
+        this.state = {
+            open: false,
+        };
+    }
+
+    handleTouchTap = (event) => {
+        // This prevents ghost click.
+        event.preventDefault();
+
+        this.setState({
+            open: true,
+            anchorEl: event.currentTarget,
+        });
     };
-  }
 
-  handleTouchTap = (event) => {
-    // This prevents ghost click.
-    event.preventDefault();
+    handleRequestClose = () => {
+        this.setState({
+            open: false,
+        });
+    };
 
-    this.setState({
-      open: true,
-      anchorEl: event.currentTarget,
-    });
-  };
-
-  handleRequestClose = () => {
-    this.setState({
-      open: false,
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <RaisedButton
-          onTouchTap={this.handleTouchTap}
-          label="Click me"
-        />
-        <Popover
-          open={this.state.open}
-          anchorEl={this.state.anchorEl}
-          anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
-          targetOrigin={{horizontal: 'left', vertical: 'top'}}
-          onRequestClose={this.handleRequestClose}
-        >
-          <Menu>
-            <MenuItem primaryText="Refresh" />
-            <MenuItem primaryText="Help &amp; feedback" />
-            <MenuItem primaryText="Settings" />
-            <MenuItem primaryText="Sign out" />
-          </Menu>
-        </Popover>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <RaisedButton
+                    onTouchTap={this.handleTouchTap}
+                    label="Click me" />
+                <Popover
+                    isOpen={this.state.open}
+                    anchorElement={this.state.anchorEl}
+                    anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+                    targetOrigin={{horizontal: 'left', vertical: 'top'}}
+                    onRequestClose={this.handleRequestClose}>
+                    <h3>Content heading</h3>
+                    <p>A paragraph of content.</p>
+                </Popover>
+            </div>
+        );
+    }
 }
--- docs/src/app/components/pages/components/Popover/Page.js
+++ docs/src/app/components/pages/components/Popover/Page.js
@@ -1,44 +1,43 @@
 import React from 'react';
 import Title from 'react-title-component';
-
 import CodeExample from '../../../CodeExample';
-import PropTypeDescription from '../../../PropTypeDescription';
 import MarkdownElement from '../../../MarkdownElement';
-
 import popoverReadmeText from './README';
 import PopoverExampleSimple from './ExampleSimple';
 import popoverExampleSimpleCode from '!raw!./ExampleSimple';
-import PopoverExampleAnimation from './ExampleAnimation';
-import popoverExampleAnimationCode from '!raw!./ExampleAnimation';
-import PopoverExampleConfigurable from './ExampleConfigurable';
-import popoverExampleConfigurableCode from '!raw!./ExampleConfigurable';
 import popoverNoteText from './NOTE';
-import popoverCode from '!raw!material-ui/Popover/Popover';
+import popoverCode from '!raw!versionone-ui/Popover/Popover';
 import popoverPackage from 'versionone-ui/Popover/package.json';
+import PropTypeDescription from './../../../PropTypeDescription';
 
 const descriptions = {
-  simple: 'A simple example showing a Popover containing a [Menu](/#/components/menu). ' +
-  'It can be also closed by clicking away from the Popover.',
-  animation: 'The default animation style is to animate around the origin. ' +
-  'An alternative animation can be applied using the `animation` property. ' +
-  'Currently one alternative animation is available, `popover-animation-from-top`, which animates vertically.',
-  configurable: 'Use the radio buttons to adjust the `anchorOrigin` and `targetOrigin` positions.',
+    simple: 'A simple example showing a Popover containing a [Menu](/#/components/menu). It can be also closed by clicking away from the Popover.',
+    animation: 'The default animation style is to animate around the origin. An alternative animation can be applied using the `animation` property. Currently one alternative animation is available, `popover-animation-from-top`, which animates vertically.',
+    configurable: 'Use the radio buttons to adjust the `anchorOrigin` and `targetOrigin` positions.',
 };
 
 const PopoverPage = () => (
-  <div>
-    <Title render={(previousTitle) => `Popover - ${previousTitle}`} />
-    <MarkdownElement text={popoverReadmeText} />
-    <MarkdownElement text={popoverNoteText} />
-  </div>
+    <div>
+        <Title render={(previousTitle) => `Popover - ${previousTitle}`} />
+        <MarkdownElement text={popoverReadmeText} />
+        <CodeExample
+            title="Simple example"
+            description={descriptions.simple}
+            code={popoverExampleSimpleCode}>
+            <PopoverExampleSimple />
+        </CodeExample>
+        <MarkdownElement text={popoverNoteText} />
+        <PropTypeDescription header="## PropTypes" code={popoverCode} />
+    </div>
 );
 export default PopoverPage;
 export const meta = {
     ...popoverPackage,
     title: 'Popover',
-    keywords: [
-    ],
+    keywords: [],
     'see also': [
-        'dropdown'
+        'dropdown',
+        'tooltip',
+        'select field'
     ]
 };

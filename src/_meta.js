ab395510d0bc22c45b2ad0e71c230c9bdaae4045
Andrew Smith <andrew@andrew.codes>
2016-10-27 14:52:44 -0400

    Exclude components other than TextField from docs; they are not ready and do not conform to the theme-enabled propTypes required for docs

--- src/_meta.js
+++ src/_meta.js
@@ -1,4 +1,1 @@
-export {default as panel} from './Panel/_meta';
-export {default as popover} from './Popover/_meta';
-export {default as toolbar} from './Toolbar/_meta';
 export {default as textField} from './TextField/_meta';

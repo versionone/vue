4865bf3d8d2af8ecddd0e4c29773d97f03064c10
Andrew Smith <andrew@andrew.codes>
2016-10-28 18:32:43 -0400

    Remove components that will break build (not theme-enabled in any form or fashion)

--- src/Popover/examples/index.js
+++
@@ -1,8 +0,0 @@
-export default [
-    {
-        title: 'Simple Example',
-        description: 'A simple example showing a Popover containing a [Menu](/#/components/menu). It can be also closed by clicking away from the Popover.',
-        code: require('!raw!./SimpleExample'),
-        component: require('./SimpleExample').default
-    }
-];

68597178c3d4a94b732f54e360e41b8ae4b059a3
Andrew Smith <andrew@andrew.codes>
2016-11-20 19:50:08 -0500

    Examples

--- src/Button/examples/index.js
+++ src/Button/examples/index.js
@@ -1,2 +1,14 @@
 export default [
+    {
+        title: 'Simple Examples',
+        description: 'Examples demonstrating the key features of buttons.',
+        code: require('!raw!./SimpleExamples'),
+        component: require('./SimpleExamples').default
+    },
+    {
+        title: 'Button Types Examples',
+        description: 'Examples demonstrating the various types of buttons.',
+        code: require('!raw!./TypesExamples'),
+        component: require('./TypesExamples').default
+    }
 ];
--- src/_meta.js
+++ src/_meta.js
@@ -1,2 +1,3 @@
+export {default as button} from './Button/_meta';
 export {default as textField} from './TextField/_meta';
 export {default as theme} from './Theme/_meta';

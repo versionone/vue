ce56868977dbcf0b80c930f9060a1af54c386860
Andrew Smith <andrew@andrew.codes>
2016-11-21 12:54:43 -0500

    Properly export button sizes and types

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-export {default as Button} from './Button';
-export * as ButtonSizes from './Button/Sizes';
-export * as ButtonTypes from './Button/Types';
-export {default as TextField} from './TextField';
+import * as buttonSizes from './Button/Sizes';
+import * as buttonTypes from './Button/Types';
 
+export {default as Button} from './Button';
+export const ButtonSizes = buttonSizes;
+export const ButtonTypes = buttonTypes;
+export {default as TextField} from './TextField';
 export * from './Theme';

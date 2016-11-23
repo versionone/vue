77e25f2b685523d9043b1ca20bfdb8a022cdf9ed
Andrew Smith <andrew@andrew.codes>
2016-11-23 15:19:32 -0500

    Reformat colors so they appear without spaces in the Themes page
    ‘rgb(0, 0, 0)` => ‘rgb(0,0,0)`
    Few other clean ups

--- src/styles/themes/v1Theme/foundations/colors.js
+++ src/styles/themes/v1Theme/foundations/colors.js
@@ -1,20 +1,18 @@
 import {lighten} from 'vue/utilities/colorManipulator';
 
-// # Color definitions
-
 // ## Black, whites, and grays // Examples
 export const black = '#000000';
 export const white = '#ffffff';
-export const transparent = 'rgba(0, 0, 0, 0)';
-export const fullBlack = 'rgba(0, 0, 0, 1)';
-export const darkBlack = 'rgba(0, 0, 0, 0.87)';
-export const lightBlack = 'rgba(0, 0, 0, 0.54)';
-export const minBlack = 'rgba(0, 0, 0, 0.26)';
-export const faintBlack = 'rgba(0, 0, 0, 0.12)';
-export const fullWhite = 'rgba(255, 255, 255, 1)';
-export const darkWhite = 'rgba(255, 255, 255, 0.87)';
-export const mediumGray = 'rgba(0, 0, 0, 0.298039)';
-export const lightWhite = 'rgba(255, 255, 255, 0.5)';
+export const transparent = 'rgba(0,0,0,0)';
+export const fullBlack = 'rgba(0,0,0,1)';
+export const darkBlack = 'rgba(0,0,0,0.87)';
+export const lightBlack = 'rgba(0,0,0,0.54)';
+export const minBlack = 'rgba(0,0,0,0.26)';
+export const faintBlack = 'rgba(0,0,0,0.12)';
+export const fullWhite = 'rgba(255,255,255,1)';
+export const darkWhite = 'rgba(255,255,255,0.87)';
+export const mediumGray = 'rgba(0,0,0,0.298039)';
+export const lightWhite = 'rgba(255,255,255,0.5)';
 
 // ## Named Colors
 export const aluminum = '#878c94';

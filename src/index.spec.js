b477bbcd31fa8d9ab2c92240d1799c9ca98857fa
Andrew Smith <andrew@andrew.codes>
2016-11-23 14:10:40 -0500

    Remove withTheme, no longer needed or used

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,12 +1,8 @@
-import {Button, ButtonSizes, ButtonTypes, TextField, ThemeProvider, withTheme} from './index';
+import {Button, ButtonSizes, ButtonTypes, TextField, ThemeProvider} from './index';
 
 suite('vue exports', () => {
     test('vue exports the ThemeProvider component', () => {
         expect(ThemeProvider).to.not.be.undefined;
-    });
-
-    test('vue exports a way to overide themes for Vue components', () => {
-        expect(withTheme).to.not.be.undefined;
     });
 
     test('vue exports the TextField component', () => {

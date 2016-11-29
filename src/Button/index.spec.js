d46253d07fd481bb3b2443c4a331281d5bba2fd9
Andrew Smith <andrew@andrew.codes>
2016-11-29 17:58:54 -0500

    Proper name of test case for exporting button externally to Vue

--- src/Button/index.spec.js
+++ src/Button/index.spec.js
@@ -1,7 +1,7 @@
 import Button from './';
 import ButtonComponent from './Button';
 
-suite('Button index', () => {
+suite('Button', () => {
     test('it exports a Button component as the default export', () => {
         expect(new Button()).to.be.an.instanceof(ButtonComponent);
     });

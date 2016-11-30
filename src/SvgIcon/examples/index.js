b54810cdb4b473ab6999dd9bcfd7f4496eb81dd3
Andrew Smith <andrew@andrew.codes>
2016-11-29 23:37:39 -0500

    S-60557 Export SvgIcon for usage in an example for docs site

--- src/SvgIcon/examples/index.js
+++ src/SvgIcon/examples/index.js
@@ -1,2 +1,8 @@
 export default [
+    {
+        title: 'Creating a Custom SVG Icon',
+        description: 'Example demonstrating the creation of a custom SVG icon.',
+        code: require('!raw!./CustomSvgIcon'),
+        component: require('./CustomSvgIcon').default
+    }
 ];

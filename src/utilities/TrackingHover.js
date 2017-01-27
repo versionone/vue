44561736bebbe075d9ea872b35bc3f994389e350
Andrew Smith <andrew@andrew.codes>
2017-01-26 21:31:43 -0500

    Rename to something more appropriate
    - used when tracking hover state and pushing a hovered prop into the wrapper component

---
+++ src/utilities/TrackingHover.js
@@ -0,0 +1,47 @@
+import React, {Component, PropTypes} from 'react';
+
+export default (ComponentToTrackHover) => {
+    return class TrackingHover extends Component {
+        static propTypes = Component.propTypes;
+
+        constructor(...rest) {
+            super(...rest);
+            this.handleMouseEnter = this.handleMouseEnter.bind(this);
+            this.handleMouseLeave = this.handleMouseLeave.bind(this);
+
+            this.state = {
+                hovered: false,
+            };
+        }
+
+        handleMouseEnter() {
+            this.setState({
+                hovered: true,
+            });
+        }
+
+        handleMouseLeave() {
+            this.setState({
+                hovered: false,
+            });
+        }
+
+        render() {
+            const {
+                hovered
+            } = this.state;
+
+            return (
+                <div
+                    onMouseEnter={this.handleMouseEnter}
+                    onMouseLeave={this.handleMouseLeave}
+                >
+                    <ComponentToTrackHover
+                        {...this.props}
+                        hovered={hovered}
+                    />
+                </div>
+            )
+        }
+    };
+};

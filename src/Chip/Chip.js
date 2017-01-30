0081316664d263aae7a5b9632a2880fb825cadab
Andrew Smith <andrew@andrew.codes>
2017-01-29 19:05:47 -0500

    Add documentation for Chip prop types

--- src/Chip/Chip.js
+++ src/Chip/Chip.js
@@ -91,17 +91,44 @@ const Chip = (props, context) => {
     );
 };
 Chip.propTypes = {
+    /**
+     * Background color of the chip
+     */
     backgroundColor: PropTypes.string,
+    /**
+     * Font color of the chip
+     */
     color: PropTypes.string,
+    /**
+     * Font size used for the text of the Chip
+     */
     fontSize: PropTypes.string,
+    /**
+     * The Chip will expand to fill the container's space if true; otherwise it will inline-block like with a width
+     */
     fullWidth: PropTypes.bool,
+    /**
+     * Line height for the text of the Chip
+     */
     lineHeight: PropTypes.number,
+    /**
+     * The unique identifier for the text of this Chip; useful when needing to reference a specific Chip for `onRequestRemove` callback
+     */
     oid: PropTypes.oneOfType([
         PropTypes.string,
         PropTypes.number,
     ]),
+    /**
+     * The text to render on the Chip
+     */
     text: PropTypes.string.isRequired,
+    /**
+     * Width of the Chip
+     */
     width: PropTypes.number,
+    /**
+     * Function handling the clicking of the close icon of the Chip
+     */
     onRequestRemove: PropTypes.func,
 };
 Chip.contextTypes = { theme: PropTypes.shape(ThemeProvider.themeDefinition).isRequired, };

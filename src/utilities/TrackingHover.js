b674497323bc78c1de4f6088dcd1100da0ac15b0
Andrew Smith <andrew@andrew.codes>
2017-01-29 19:05:09 -0500

    HoC needs to pass context down correctly

--- src/utilities/TrackingHover.js
+++ src/utilities/TrackingHover.js
@@ -1,7 +1,15 @@
-import React, { Component, } from 'react';
+import React, { Component, PropTypes, } from 'react';
+import ThemeProvider from './../Theme';
 
 export default ComponentToTrackHover => class TrackingHover extends Component {
     static propTypes = Component.propTypes;
+    static contextTypes = { theme: PropTypes.shape(ThemeProvider.themeDefinition).isRequired, };
+    static
+    childContextTypes = { theme: PropTypes.shape(ThemeProvider.themeDefinition).isRequired, };
+
+    getChildContext() {
+        return this.context;
+    }
 
     constructor(...rest) {
         super(...rest);

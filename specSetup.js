936e48f822d346e0d80b4fa3d72ca86399c471b4
Andrew Smith <andrew@andrew.codes>
2017-02-03 11:05:34 -0500

    Attempt to speed up running specs and weird errors when running in CLI that do not happen when running via wallabyjs.
    - Spiking jest and snapshot tests
    	- snapshop testing seems particularly suited for some of the UI tests

--- specSetup.js
+++
@@ -1,8 +0,0 @@
-const chai = require('chai');
-const chaiEnzyme = require('chai-enzyme');
-const jsdom = require('jsdom').jsdom;
-import initializeGlobalWindow from './specHelpers/initializeGlobalWindow';
-
-chai.use(chaiEnzyme());
-global.expect = chai.expect;
-initializeGlobalWindow();
--- wallaby.js
+++ wallaby.js
@@ -1,43 +1,31 @@
-process.env.NODE_ENV = 'test';
-process.env.BABEL_ENV = 'test';
-
 module.exports = function(wallaby) {
     return {
         files: [
-            {pattern: 'specSetup.js'},
-            {pattern: 'specHelpers/**/*.js'},
-            {pattern: 'src/**'},
-            {pattern: '!src/**/examples/**/*.*'},
-            {pattern: '!src/**/Stories.js'},
-            {pattern: '!src/**/_meta.js'},
-            {pattern: '!src/**/*.spec.js'}
+            'src/**/*.js',
+            'package.json',
+            '!src/**/*.test.js',
+            '!src/**/examples/**/*.*',
+            '!src/**/Stories.js',
+            '!src/**/_meta.js',
+            '!src/**/*.spec.js',
         ],
         tests: [
-            {pattern: 'src/**/*.spec.js'}
+            'src/**/__tests__/*.test.js',
         ],
-        filesWithNoCoverageCalculated: [
-            'specSetup.js',
-            'specHelpers/**/*.js'
-        ],
-        compilers: {
-            '**/*.js': wallaby.compilers.babel()
-        },
         env: {
             type: 'node',
-            runner: 'node'
+            runner: 'node',
         },
-        testFramework: 'mocha',
-        bootstrap: function(wallaby) {
-            wallaby.testFramework.ui('tdd');
-            const jsdom = require('jsdom').jsdom;
-            global.document = jsdom('<!doctype html><html><body></body></html>');
-            global.window = document.defaultView;
-            global.navigator = {
-                userAgent: 'node.js'
-            };
-            var path = require('path');
-            require('./specSetup');
+        filesWithNoCoverageCalculated: [
+            'specSetup.js',
+            'specHelpers/**/*.js',
+        ],
+        compilers: {
+            '**/*.js': wallaby.compilers.babel(),
         },
-        debug: true
+        testFramework: 'jest',
+        setup: function(wallaby) {
+            wallaby.testFramework.configure(require('./package.json').jest);
+        },
     };
 };

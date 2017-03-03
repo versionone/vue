124e56764c3ef33e3a367831942a82f217d953d4
Andrew Smith <andrew@andrew.codes>
2017-03-03 12:50:43 -0500

    VueProvider will now automatically create a store if there is not one provided already in the context. This allows the consumer to not have to create a redux store with uiReducer; they may not even use redux.

--- src/VueProvider/VueProvider.js
+++ src/VueProvider/VueProvider.js
@@ -1,23 +1,33 @@
 import React, {PropTypes} from 'react';
 import {Provider} from 'react-redux';
+import {combineReducers, createStore} from 'redux';
+import {reducer as uiReducer} from 'redux-ui';
 import ThemeProvider from './../ThemeProvider';
 import V1Provider from './../V1Provider';
 
-const VueProvider = (props, context) => (
-    <V1Provider runQuery={props.runQuery}>
-        <ThemeProvider theme={props.theme}>
-            <Provider store={context.store ? context.store : props.store}>
-                {props.children}
-            </Provider>
-        </ThemeProvider>
-    </V1Provider>
-);
+const VueProvider = (props, context) => {
+    let store = context.store;
+    if (!store) {
+        store = createStore(combineReducers({
+            ui: uiReducer,
+        }));
+    }
+    return (
+        <V1Provider runQuery={props.runQuery}>
+            <ThemeProvider theme={props.theme}>
+                <Provider store={store}>
+                    {props.children}
+                </Provider>
+            </ThemeProvider>
+        </V1Provider>
+    );
+};
 // VueProvider.displayName = 'VueProvider';
 VueProvider.propTypes = {
     /**
      * Instance of the VersionOne JavaScript SDK
      */
-    runQuery: PropTypes.func.isRequired,
+    runQuery: PropTypes.func,
     /**
      * A redux store with `ui` state segment property.
      */
@@ -28,6 +38,11 @@ VueProvider.propTypes = {
     /**
      * Theme to be used with Vue
      */
-    theme: PropTypes.shape(ThemeProvider.themeDefinition).isRequired,
+    theme: PropTypes.shape(ThemeProvider.themeDefinition),
+};
+VueProvider.defaultProps = {
+  runQuery: () => {
+      console.warn('You have not specified a runQuery function in `VueProvider`.');
+  },
 };
 export default VueProvider;

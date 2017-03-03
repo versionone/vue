6c5afeff5a93a43aea8c7090a2d6f13c72d1071d
Andrew Smith <andrew@andrew.codes>
2017-03-03 12:32:17 -0500

    Remove AssetLookup from Lookup directory

--- src/Lookup/__tests__/index.test.js
+++ src/Lookup/__tests__/index.test.js
@@ -1,13 +1,8 @@
 import Lookup, {AssetLookup} from './../';
-import AssetLookupComponent from '../AssetLookup';
 import LookupComponent from '../Lookup';
 
 test('a Lookup component is exported', () => {
     expect(new Lookup({
         selectedItems: [],
     })).toBeInstanceOf(LookupComponent);
-});
-
-test('an AssetLookup component is exported', () => {
-    expect(AssetLookup).toEqual(AssetLookupComponent);
 });
--- src/Lookup/index.js
+++ src/Lookup/index.js
@@ -1,4 +1,3 @@
 import Lookup from './Lookup';
 
 export default Lookup;
-export {default as AssetLookup} from './AssetLookup';

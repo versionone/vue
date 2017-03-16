a7aa161b6e68ef849b344391729988ac313b2b4a
codeEmitter <greg.johnson@versionone.com>
2017-03-16 16:12:56 -0400

    S-61828 remove class attribute from svgs to avoid React warnings

--- src/Icons/CheckboxUncheckedIcon.js
+++ src/Icons/CheckboxUncheckedIcon.js
@@ -3,7 +3,7 @@ import SvgIcon from './../SvgIcon';
 
 const CheckboxUncheckedIcon = props => (
     <SvgIcon {...props}>
-        <path class="st0" d="M11.5,13.5h-7c-1.1,0-2-0.9-2-2v-7c0-1.1,0.9-2,2-2h7c1.1,0,2,0.9,2,2v7C13.5,12.6,12.6,13.5,11.5,13.5z"/>
+        <path d="M11.5,13.5h-7c-1.1,0-2-0.9-2-2v-7c0-1.1,0.9-2,2-2h7c1.1,0,2,0.9,2,2v7C13.5,12.6,12.6,13.5,11.5,13.5z"/>
     </SvgIcon>
 );
 CheckboxUncheckedIcon.displayName = 'CheckboxUncheckedIcon';

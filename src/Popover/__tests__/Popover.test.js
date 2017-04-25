925f78ece9924add23f169fbc6c3c8440a9eb12b
Andrew Smith <andrew@andrew.codes>
2017-04-25 17:10:43 -0400

    Update tests: popovers now require an anchor to render properly
    - it is not sufficient to simply say it is open/closed

--- src/Popover/__tests__/Popover.test.js
+++ src/Popover/__tests__/Popover.test.js
@@ -1,14 +1,19 @@
 import Popover from './../Popover';
 import {getMount, reset} from './../../../specHelpers/rendering';
 
-window.getComputedStyle = jest.fn()
-    .mockReturnValue({
-        borderLeftWidth: '0px',
-        borderRightWidth: '0px',
-    });
-
-afterEach(reset());
 const mountPopover = getMount(Popover);
+let anchorElement;
+
+beforeEach(() => {
+    window.getComputedStyle = jest.fn()
+        .mockReturnValue({
+            borderLeftWidth: '0px',
+            borderRightWidth: '0px',
+        });
+    anchorElement = document.createElement('div');
+    document.body.appendChild(anchorElement);
+});
+afterEach(reset());
 
 test('Popover will not be visible when closed or unspecified open state', () => {
     mountPopover({
@@ -20,8 +25,16 @@ test('Popover will not be visible when closed or unspecified open state', () => 
     expect(popoverIsNotVisible()).toBeTruthy();
 });
 
+test('will not render when anchor element is detached from document', () => {
+    mountPopover({
+        anchorElement,
+        open: true,
+    })
+});
+
 test('Popovers can be opened', () => {
     mountPopover({
+        anchorElement,
         open: true,
     });
     expect(popoverIsVisible()).toBeTruthy();
@@ -29,6 +42,7 @@ test('Popovers can be opened', () => {
 
 test('Popovers can be toggled from closed to opened', () => {
     const popover = mountPopover({
+        anchorElement,
         open: false,
     });
     popover.setProps({
@@ -39,6 +53,7 @@ test('Popovers can be toggled from closed to opened', () => {
 
 test('Popovers can be toggled from open to closed', () => {
     const popover = mountPopover({
+        anchorElement,
         open: true,
     });
     popover.setProps({
@@ -49,6 +64,7 @@ test('Popovers can be toggled from open to closed', () => {
 
 test('Popovers can can contain content when opened', () => {
     mountPopover({
+        anchorElement,
         children: 'Hello world',
         open: true,
     });
@@ -67,5 +83,5 @@ function popoverIsVisible() {
     return getRootElementOfPopover() !== undefined;
 }
 function popoverToContainContent() {
-    return getRootElementOfPopover().innerHTML === 'Hello world';
+    return getRootElementOfPopover().children[0].innerHTML === 'Hello world';
 }

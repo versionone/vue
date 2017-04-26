0fca34922da91397e5a315b92db3e07ab9b242fc
Andrew Smith <andrew@andrew.codes>
2017-04-26 09:22:25 -0400

    Minor clean-up of script tasks

--- scripts/directories.js
+++ scripts/directories.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var path = require('path');
 var fs = require('fs');
 
@@ -5,8 +7,8 @@ var app = path.join(__dirname, '..');
 module.exports.app = app;
 module.exports.src = path.join(app, 'src');
 
-module.exports.isDirectory = function(rootDirectory) {
-    return function(itemRef) {
+module.exports.isDirectory = function isDirectory(rootDirectory) {
+    return function isDirectorySpecification(itemRef) {
         return fs.statSync(path.join(rootDirectory, itemRef)).isDirectory();
     };
 };

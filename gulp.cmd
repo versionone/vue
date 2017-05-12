13a4e45ef91a63c201b06cd377b2b2e973ddd550
Andrew Smith <andrew@andrew.codes>
2017-05-12 11:22:30 -0400

    Gulp clean task.
    Introduce gulp to help better tie versioning and publishing into a single task to eliminate errors when publishing. These errors include versioning, modifying built files, then publishing; as opposed to versioning and then immediately publishing.

---
+++ gulp.cmd
@@ -0,0 +1,1 @@
+"node.exe" "%~dp0\node_modules\gulp\bin\gulp.js" %*
--- scripts/clean.js
+++ scripts/clean.js
@@ -1,35 +1,16 @@
-#!/usr/bin/env node
+const del = require('del');
+const gulp = require('gulp');
 
-const fs = require('fs');
-const path = require('path');
-const rimraf = require('rimraf');
-const dirs = require('./directories');
-
-fs.readdirSync(dirs.src)
-    .map(toDeployedDirectory(dirs.app))
-    .filter(hasMatchingDeployedDirectory())
-    .forEach(deleteDeployedDirectory());
-
-function toDeployedDirectory(deployRootDirectory) {
-    return function(directoryRef) {
-        return path.join(deployRootDirectory, directoryRef);
-    };
-}
-
-function hasMatchingDeployedDirectory() {
-    return function(directoryRef) {
-        return fs.existsSync(directoryRef);
-    };
-}
-
-function deleteDeployedDirectory() {
-    return function(directoryRef) {
-        rimraf(path.join(directoryRef), logDone(directoryRef));
-    }
-}
-
-function logDone(directoryRef) {
-    return function() {
-        console.log('cleaned', directoryRef);
-    }
-}
+gulp.task('clean', () => del([
+    '*',
+    '!.storybook',
+    '!docs',
+    '!node_modules',
+    '!packages',
+    '!scripts',
+    '!specHelpers',
+    '!src',
+    '!gulp',
+    '!*.*',
+    'index.js',
+]));

209f63741c241ed3f76ef8081654faf0e4218225
Andrew Smith <andrew@andrew.codes>
2017-05-12 16:38:56 -0400

    Publishing with a version type (-t or —type) option

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,36 +1,95 @@
-#!/usr/bin/env node
-//
-// const execSync = require('child_process').execSync;
-// const path = require('path');
-//
-// const version = process.argv[2];
-//
-// if (!version) {
-//     console.log('Version not specified (patch | minor | major | prepatch | preminor | premajor | prerelease | 1.2.3)');
-//     console.log('Example: npm run pub patch');
-//     process.exitCode = 1;
-//     return;
-// }
-//
-// if (version === 'next') {
-//     const currentVersion = require('./../package.json').version;
-//     console.log(`Publishing as an update to ${currentVersion}-next.`);
-//     console.log('This will not publish the docs site.');
-//     execSync(`./node_modules/.bin/np ${currentVersion} --tag next`);
-// } else {
-//     console.log(`Publishing as a new ${version} version.`);
-//     execSync(`./node_modules/.bin/np ${version}`);
-// }
-//
-// if (shouldPublishDocs(version)) {
-//     process.chdir(path.join(__dirname, '..', 'docs'));
-//     execSync('npm run gh-pages:build -p');
-// }
-//
-// function shouldPublishDocs(publishedVersion) {
-//     const docPublishableVersions = [
-//         'minor',
-//         'major',
-//     ];
-//     return docPublishableVersions.indexOf(publishedVersion) > -1;
-// }
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const path = require('path');
+const sequence = require('run-sequence');
+const yargs = require('yargs');
+const exec = require('./scriptHelpers/exec');
+
+const getVersionToPublish = () => {
+    const params = yargs
+        .option('type', {
+            alias: 't',
+            describe: 'version type; one of [patch, minor, major, next]',
+        })
+        .demandOption([
+            'type',
+        ])
+        .help()
+        .fail((msg, error, y) => {
+            if (error) {
+                throw new gutil.PluginError({
+                    message: error,
+                    plugin: 'publish',
+                });
+            }
+            console.error(msg);
+            console.error('You should be doing', y.help());
+            process.exit(1);
+        })
+        .exitProcess(false)
+        .argv;
+
+    return params.type;
+};
+
+const isInvalidVersion = version => [
+    'patch',
+    'minor',
+    'major',
+    'next',
+].indexOf(version) === -1;
+
+const version = versionToPublish => new Promise((resolve, reject) => {
+    if (isInvalidVersion(versionToPublish)) {
+        reject(`Invalid version provided: ${versionToPublish}`);
+        return null;
+    }
+    if (versionToPublish === 'next') {
+        const currentVersion = require('./../package.json').version;
+        console.log(`Publishing as an update to ${currentVersion}-next.`);
+        console.log('This will not publish the docs site.');
+        return exec(`npm version ${currentVersion} --tag next`);
+    }
+    console.log(`Publishing as a new ${versionToPublish} version.`);
+    return exec(`npm version ${versionToPublish}`);
+});
+
+const publish = () => exec('npm publish');
+
+const shouldNotPublishDocs = versionToPublish => [
+    'minor',
+    'major',
+].indexOf(versionToPublish) === -1;
+
+const publishDocs = versionToPublish => new Promise((resolve) => {
+    if (shouldNotPublishDocs(versionToPublish)) {
+        resolve();
+        return null;
+    }
+
+    process.chdir(path.join(process.cwd, 'docs'));
+    return exec('npm run gh-pages:build'); // -p
+});
+
+gulp.task('publish', () => {
+    const versionToPublish = getVersionToPublish();
+    if (isInvalidVersion(versionToPublish)) {
+        throw new gutil.PluginError({
+            message: `Invalid version type ${versionToPublish}. Must be one of [patch, minor, major, next]`,
+            plugin: 'publish/src',
+        });
+    }
+    return sequence('publish/src', 'clean');
+});
+
+gulp.task('publish/src', [
+    'lint',
+    'test',
+    'build',
+], () => {
+    const versionToPublish = getVersionToPublish();
+
+    return version(versionToPublish)
+        .then(publish())
+        .then(publishDocs(versionToPublish));
+});

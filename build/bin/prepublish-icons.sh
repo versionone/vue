f6bbf9bd257fd622daa402bfc9fe449dac859587
Andrew Smith <andrew@andrew.codes>
2017-05-25 19:11:53 -0400

    Publish icons to packages/icons/src

---
+++ build/bin/prepublish-icons.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+pushd ../.. > /dev/null
+export PATH="`yarn bin`:$PATH"
+popd > /dev/null
+
+chalk --no-stdin -t "{blue Generating icons...}"
+node packages/v1-icon-builder/src/build.js --svgDir=packages/v1-icons/src --outputDir=packages/Icons/src

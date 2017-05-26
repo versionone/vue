2547fa2c27c178906276dcb9b89cadfee8e8800e
Andrew Smith <andrew@andrew.codes>
2017-05-26 14:47:22 -0400

    EOL endings fix

--- build/storybook/config.js
+++ build/storybook/config.js
@@ -23,7 +23,7 @@ addDecorator((story) => (
     </div>
 ));
 
-const req = require.context('./../', true, /Stories\.js$/);
+const req = require.context('./../../packages', true, /stories\/.*\.js$/);
 
 function loadStories() {
     req.keys().forEach(req);

b90d80e7792da219fd24abcf5020e134c191cbbf
Erin Stanfill <erin.stanfill@versionone.com>
2017-12-01 15:11:58 -0500

    v4.0.3

---
+++ VersionOneUIProvider/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _VersionOneUIProvider = require('./VersionOneUIProvider');
+
+var _VersionOneUIProvider2 = _interopRequireDefault(_VersionOneUIProvider);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _VersionOneUIProvider2.default;
